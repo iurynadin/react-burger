492d24daf7400aa38968690e0dbc1193f900ee7b
iury nadin <iurynadin@gmail.com>
2020-06-15 09:02:57 -0300

    17.5

--- src/containers/BurguerBuilder/BurguerBuilder.jsx
+++ src/containers/BurguerBuilder/BurguerBuilder.jsx
@@ -20,13 +20,13 @@ class BurguerBuilder extends Component {
 
     componentDidMount() {
         console.log(this.props);
-        // axios.get('https://burguer-builder-8ce37.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ ingredients: response.data  });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ error:true  });
-        //     });
+        axios.get('https://burguer-builder-8ce37.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ ingredients: response.data  });
+            })
+            .catch(error => {
+                this.setState({ error:true  });
+            });
     }
 
     updatePurchaseState(ingredients) {
