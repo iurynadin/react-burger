95893f2012b218adf1d45615a4a4ea49f67403ba
iury nadin <iurynadin@gmail.com>
2020-06-21 18:27:08 -0300

    18.6

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-// import axios from '../../axios-orders';
+import axios from 'axios';
 
 
 export const authStart = () => {
@@ -25,5 +25,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            reurnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0FSDlFNJdVfjlfxXE43tafQ6tJGDGTWA', authData)
+            .then(response =>{
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch( err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
+
     };
 }
